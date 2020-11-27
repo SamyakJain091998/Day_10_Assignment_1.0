3a2f3c5ef09147594de2fbb86cab6096034267b8
SamyakJain091998 <2016224@iiitdmj.ac.in>
2020-11-27 21:35:30 +0530

    Changes made

--- src/com/javaStream/practice/Address_Book.java
+++ src/com/javaStream/practice/Address_Book.java
@@ -7,6 +7,7 @@ import java.util.function.Predicate;
 
 public class Address_Book {
 	private ArrayList<Contacts> arrList = new ArrayList<Contacts>();
+
 //	Contacts dummyCon = new Contacts();
 //	
 //	Predicate<Contacts> isExist = dummyCon -> {
@@ -16,17 +17,18 @@ public class Address_Book {
 //	};
 	public void addToAddressBook(Contacts con) {
 //		System.out.println("arrList.contains(con) : " + arrList.contains(con));
-		if(arrList.stream().anyMatch(dummy -> arrList.contains(con))) {
+		if (arrList.stream().anyMatch(dummy -> dummy.equals(con))) {
 			System.out.println("Sorry! The details already exist...");
 			System.exit(0);
-		};
+		}
+		;
 //		if(arrList.contains(con)) {
 //			System.out.println("Sorry! The details already exist...");
 //			System.exit(0);
 //		}
 		arrList.add(con);
 	}
-	
+
 	public void printArrayList() {
 		System.out.println("Updated array list is: ");
 		System.out.println(arrList);
